79f78abeb1f9b66eec1a97aa741ddf35eee50264
William Hutt <whutt@csumb.edu>
2017-09-12 02:08:46 +0000

    Lab2 finished

--- 777/index.php
+++ 777/index.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php
+        
+        include 'inc/functions.php';
+    
+        
+        ?>
         <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
-
+        
+        <div id="main">
+            <?php
+                play();
+            ?>
+            
+            <form>
+                <input type="submit" value="spin" />
+            </form>
+        </div>    
     </body>
 </html>
