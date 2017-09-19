9713160fce3e5ff75e87583a3f52b8bfb7246a7a
William Hutt <whutt@csumb.edu>
2017-09-19 21:50:24 +0000

    working on hw2

--- index.php
+++ index.php
@@ -8,8 +8,13 @@
         
         <?php
             
+            $Dice = array (
+                "diceImages" => "images/dice/",
+                "dice" => rand(1,6) .".png");
             
-            
+            for ($i=1;$i <7;$i++) {
+               echo "<img src='".$Dice["diceImages"] .$i.'.png' width="200px" height="200px".'>";
+            }
 
 
         ?>
