e2c01caac3ab1685325a1565f5084948e4c3f3f2
William Hutt <whutt@csumb.edu>
2017-09-19 21:17:53 +0000

    hw2 branch

--- index.php
+++ index.php
@@ -9,62 +9,7 @@
         <?php
             
             
-            $person = array(
-                "name" => "Will",
-                "profilePicUrl" => "cards/profile_pics/will.png",
-                "cards" => array(
-                    array("suit" => "hearts",
-                    "value" => rand(1,13)
-                        ),
-                    array("suit" => "clubs",
-                    "value" => rand(1,13)
-                        )
-                    )
-                );
-                
-                
-            function displayPerson($person) {
-                // show profile pic
-                echo "<img src='".$person["profilePicUrl"]."'>";
-                
-                // iterate through $persons "cards"
-                
-                for($i=0;$i< count($person["cards"]);$i++) {
-                    $card = $person["cards"][$i];
-                    
-                    // construct the imgURL for each card
-                    $imgURL = "cards/" .$card["suit"]. "/" .$card["value"]. ".png";
-                    
-                    // translate this to HTML
-                    echo "<img src='".$imgURL."'>";
-                    
-                }
-                
-                echo calculateHandValue($person["cards"]);
-                
-            }    
             
-            // calculates hand value
-            function calculateHandValue($cards) {
-                $sum = 0;
-                for($i = 0; $i < count($cards); $i++) {
-                    $sum += $cards[$i]["value"];
-                }
-                
-                return $sum;
-            }
-            displayPerson($person);
-            
-            
-            
-            // detects win or lose
-            function winning($sum) {
-                if ($sum != 42) {
-                    displayPerson($person);
-                } else {
-                    echo "Winner!";
-                }
-            }
 
 
         ?>
