2544a7e431dac43506c31253c0d3e9cb8180d972
William Hutt <whutt@csumb.edu>
2017-10-02 21:20:34 +0000

    working somewhat

--- Slider/index.php
+++ Slider/index.php
@@ -1,56 +1,61 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
+ <?php
+    $backgroundImage = "img/sea.jpg";
+    
+    //API call goes here
+    if (isset($_GET['keyword'])) {
+        // echo "You searched for: " .$_GET['keyword'];
+        include 'api/pixabayAPI.php';
+        $imageURLs = getImageURLs($_GET['keyword']);
+        // print_r($imageURLs);
+        $backgroundImage = $imageURLs[array_rand($imageURLs)];
+    }
+ ?>
 <html>
     <head>
+        <title>Image Carousel</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
         <style>
-            @import url("css/style.css");
+            @import url("css/styles.css");
             body {
                 background-image: url('<?=$backgroundImage ?>');
             }
-        </style>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <title>Image Carousel</title>
-         <?php
-            $backgroundImage = "img/sea.jpg";
-         ?>
+        </style>   
     </head>
     
     <body>
+        <br/> <br />
         <?php
-            if( !isset($imageURLs)) {
-                echo "<h2> Type a keyword to display a slideshow <br/> with random images from Pixabay.com </h2>";
+            if (!isset($imageURLs)) {
+                echo "<h2> Type a keyword to display a slider <br /> with random images from Pixabay.com </h2>";
             } else {
-                // Display Carousel Here
-        
-                for ($i = 0; $i < 5;$i++) {
+                //Display Carousel Here
+                for ($i=0;$i<5;$i++) {
                     do {
-                        $randomIndex = rand(0, count($imageURLs));
+                        $randomIndex = rand(0,count($imageURLs));
                     }
                     while (!isset($imageURLs[$randomIndex]));
                     
-                    echo "<img src='" . $imageURLs[rand(0,count($imageURLs))] . "' width='200' >";
-                    unset($imageURLs[$randomIndex]);                
+                    echo "<img src='" . $imageURLs[$randomIndex] . "' width='200' >";
+                    unset ($imageURLs[$randomIndex]);
                 }
-            }
+            
         ?>
         
+        <h1>I'm a regular HTML tag inside the PHP else statement!</h1>
+
+        <?php
+            }
+        ?>    
+
+        <!-- HTML form goes here! -->
         <form>
             <input type="text" name="keyword" placeholder="Keyword">
             <input type="submit" value="Submit" />
         </form>
-        
-        <?php
-            if (isset($_GET['keyword'])) {
-                echo "You searched for: " . $_GET['keyword'];
-            }
-            if (isset($_GET['keyword'])) {
-                include 'api/pixabayAPI.php';
-                $imageURLs = getImageURLs($_GET['keyword']);
-                $backgroundImage = $imageURLs[array_rand($imageURLs)];
-            }
-            
-        ?>
-        
+        <br/> <br />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </body>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
+    </body>    
 </html>
