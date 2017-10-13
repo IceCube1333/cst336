a344e0cf807adf60edde8ad76dbc9f9c941e2d28
William Hutt <whutt@csumb.edu>
2017-10-13 23:21:17 +0000

    hw3 in progress

--- hw3/index.php
+++ hw3/index.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
     </head>
     
     <body>
+        <header>
+            <h1>Which Penguin Are You?</h1>
+        </header>
+        
+        <form>
+            <div>
+                <p id="ftitle">First Name: </p>
+                <input type="text" name="firstname" id="fname"/>
+                <br/>
+                <p id="ltitle">Last Name:</p>
+                <input type ="text" name ="lastname" id="lname"/>
+                <br/>
+                <input type ="radio" name="gender" value="Male" id="male"/>
+                <p id="mtitle">Male</p>
+                <input type="radio" name="gender" value="Female" id="female"/>
+                <p id="fetitle">Female</p><br/>
+                <input type="submit" value="Submit" id="submit"/>
+            </div>
+        </form>
+        
+        <?php
+        
+        ?>
+        
+        <footer>
+            <hr>
+            CST336 Hutt &copy;2017 
+        </footer>
         
     </body>
     
