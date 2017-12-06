94250b334645cfceb8063d949615c54563531815
William Hutt <whutt@csumb.edu>
2017-12-06 22:50:48 +0000

    more database

--- database.php
+++ database.php
@@ -6,7 +6,7 @@
         $host = "us-cdbr-iron-east-05.cleardb.net";
         $username = "b38412b4d2cec8";
         $password = "8b8683b0";
-        $dbname = "heroku_4432619d892eb4f";
+        $dbname = "heroku_62ec3967cd64e9c";
         
         // Create connection
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
